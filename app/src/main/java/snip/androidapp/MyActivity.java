071904cb1b0d36deea90286660a03c441641c21c
Ran Reichman <ranreichman@ReichmanPC.lan>
2016-07-19 12:55:56 +0300

    Start of Android app - now with everything

---
+++ app/src/main/java/snip/androidapp/MyActivity.java
@@ -0,0 +1,15 @@
+package snip.androidapp;
+
+import android.app.Activity;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by ranreichman on 7/19/16.
+ */
+public class MyActivity extends Activity {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+}
+
